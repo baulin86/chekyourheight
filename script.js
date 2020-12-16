b7ee8f8a98fc1cf759fa7f824d567c130318a945
Aleksandr <baulin1986@gmail.com>
2020-12-16 09:16:39 +0300

    add check congition of age

--- script.js
+++ script.js
@@ -23,7 +23,9 @@ function start(){
 		"ageUp26":[17.5,19.5,23,27.5,30,35,40],
 		"ageAfter26":[18,20,26,28,31,36,41],
 		showResult: function() {
-			if (this.characteriseOfIndex !=""){
+			if ((this.characteriseOfIndex == null)){
+				resultWrapper.textContent = `Нет данных для вашего возраста`;
+			}else{
 				resultWrapper.textContent = `У Вас ${this.characteriseOfIndex}`;
 			}
 		},
@@ -41,6 +43,7 @@ function start(){
 			}
 		},
 		getIndex: function(){
+			console.log(`weight ${this.weight} , height ${this.height}`)
 			this.index = +this.weight / (this.height**2);
 		},
 		checkIndex: function(){
@@ -50,32 +53,37 @@ function start(){
 			}else {
 				i = this["ageAfter26"];
 			}
-
-			if (this.index < i[0]){
-				this.characteriseOfIndex = "недостаточный вес, опасно для здоровья";
-				this.color = "red";
-			}else if(this.index < i[1]){
-				this.characteriseOfIndex = "вес слегка снижен, неопасно для здоровья";
-				this.color = "orange";
-			}else if(this.index < i[2]){
-				this.characteriseOfIndex = "вес нормальный";
-				this.color = "green";
-			}else if(this.index < i[3]){
-				this.characteriseOfIndex = "вес излишний";
-				this.color = "orange";
-			}else if(this.index < i[4]){
-				this.characteriseOfIndex = "ожирение 1 степени";
-				this.color = "red";
-			}else if(this.index < i[5]){
-				this.characteriseOfIndex = "ожирение 2 степени";
-				this.color = "red";
-			}else if(this.index < i[6]){
-				this.characteriseOfIndex = "ожирение 3 степени";
-				this.color = "red";
+			if (this.age >= 18 && this.age <= 46){
+				if (this.index < i[0]){
+					this.characteriseOfIndex = "недостаточный вес, опасно для здоровья";
+					this.color = "red";
+				}else if(this.index < i[1]){
+					this.characteriseOfIndex = "вес слегка снижен, неопасно для здоровья";
+					this.color = "orange";
+				}else if(this.index < i[2]){
+					this.characteriseOfIndex = "вес нормальный";
+					this.color = "green";
+				}else if(this.index < i[3]){
+					this.characteriseOfIndex = "вес излишний";
+					this.color = "orange";
+				}else if(this.index < i[4]){
+					this.characteriseOfIndex = "ожирение 1 степени";
+					this.color = "red";
+				}else if(this.index < i[5]){
+					this.characteriseOfIndex = "ожирение 2 степени";
+					this.color = "red";
+				}else if(this.index < i[6]){
+					this.characteriseOfIndex = "ожирение 3 степени";
+					this.color = "red";
+				}else if (this.index >= i[6]){
+					this.characteriseOfIndex = "ожирение 4 степени";
+					this.color = "red";
+				}
 			}else{
-				this.characteriseOfIndex = "ожирение 4 степени";
+				this.characteriseOfIndex = null
 				this.color = "red";
 			}
+			
 		},
 		AddColor: function(){
 			if (resultWrapper.classList.contains("red")) {
